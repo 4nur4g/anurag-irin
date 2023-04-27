f8157312657bc2dfe2bab2e5cced7a20cb8fe4e5
Anurag Pradhan <anuragp936@gmail.com>
2023-04-27 18:01:24 +0530

    Further frontend logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,74 @@
 import UserInput from "./UserInput.jsx";
 import UserList from "./UserList.jsx";
-import {useMediaQuery, Grid, List, ListItem} from '@material-ui/core';
+import { useMediaQuery, Grid, List, ListItem } from "@material-ui/core";
 import CustomCard from "./UI/CustomCard.jsx";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: "flex",
-        justifyContent: "center",
-        // Add this line to set the background to multiple gradients
-        background: "radial-gradient(51.63% 51.63% at 50% 48.37%, rgba(157, 167, 255, 0.368968) 0%, rgba(157, 167, 255, 0.117404) 77.46%, rgba(157, 167, 255, 0) 100%)," +
-            "linear-gradient(111.57deg, #FFEA31 -34.92%, rgba(255, 234, 49, 0) 85.47%)," +
-            "linear-gradient(111.57deg, #A4FF31 -34.92%, rgba(164, 255, 49, 0.560847) 19.68%, rgba(164, 255, 49, 0.259259) 57.18%, rgba(164, 255, 49, 0) 89.42%)",
-        height: "100vh",
-    },
-    backdropFilter: "blur(5px)"
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    // Add this line to set the background to multiple gradients
+    background:
+      "radial-gradient(51.63% 51.63% at 50% 48.37%, rgba(157, 167, 255, 0.368968) 0%, rgba(157, 167, 255, 0.117404) 77.46%, rgba(157, 167, 255, 0) 100%)," +
+      "linear-gradient(111.57deg, #FFEA31 -34.92%, rgba(255, 234, 49, 0) 85.47%)," +
+      "linear-gradient(111.57deg, #A4FF31 -34.92%, rgba(164, 255, 49, 0.560847) 19.68%, rgba(164, 255, 49, 0.259259) 57.18%, rgba(164, 255, 49, 0) 89.42%)",
+    height: "100vh",
+  },
+  backdropFilter: "blur(5px)",
 
-    , untilmenu: {
-        marginTop: "160px",
-    }
+  untilmenu: {
+    marginTop: "160px",
+  },
 }));
 
 function App() {
-    const isDesktop = useMediaQuery('(min-width:600px)');
-    const classes = useStyles();
-    return (
-        <>
-            {isDesktop ? (
-                <Grid container spacing={5} className={classes.root}>
-                    <Grid item className={classes.untilmenu}>
-                        <CustomCard>
-                            <UserInput/>
-                        </CustomCard>
-                    </Grid>
-                    <Grid item className={classes.untilmenu}>
-                        <CustomCard>
-                            <UserList/>
-                        </CustomCard>
-                    </Grid>
-                </Grid>
-            ) : (
-                <List>
-                    <ListItem>
-                        <CustomCard>
-                            <UserInput/>
-                        </CustomCard>
-                    </ListItem>
-                    <ListItem>
-                        <CustomCard>
-                            <UserList/>
-                        </CustomCard>
-                    </ListItem>
-                </List>
-            )}
-        </>
-    )
+  const isDesktop = useMediaQuery("(min-width:600px)");
+  const classes = useStyles();
+
+  const [formDataList, setFormDataList] = useState([]);
+
+  function handleFormSubmit(data) {
+    setFormDataList([...formDataList, data]);
+  }
+
+  useEffect(() => {
+    // log the formDataList to the console
+    console.log(formDataList);
+  }, [formDataList]); // only run when formDataList changes
+
+  return (
+    <>
+      {isDesktop ? (
+        <Grid container spacing={5} className={classes.root}>
+          <Grid item className={classes.untilmenu}>
+            <CustomCard>
+              <UserInput onFormSubmit={handleFormSubmit} />
+            </CustomCard>
+          </Grid>
+          <Grid item className={classes.untilmenu}>
+            <CustomCard>
+              <UserList userData={formDataList} />
+            </CustomCard>
+          </Grid>
+        </Grid>
+      ) : (
+        <List>
+          <ListItem>
+            <CustomCard>
+              <UserInput onFormSubmit={handleFormSubmit} />
+            </CustomCard>
+          </ListItem>
+          <ListItem>
+            <CustomCard>
+              <UserList userData={formDataList} />
+            </CustomCard>
+          </ListItem>
+        </List>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,81 +1,111 @@
-import {useState} from "react";
+/* eslint-disable react/prop-types */
+import { useState } from "react";
 import TextField from "./UI/TextField.jsx";
-import Button from "./UI/Button.jsx"
-import {makeStyles} from "@material-ui/core/styles";
-import {Box, Typography} from "@mui/material";
-
+import Button from "./UI/Button.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        maxWidth: 514
-    }, fields: {
-        display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContents: 'center'
-    }, button1: {
-        marginLeft: "24px"
-    }, button2: {
-        marginLeft: "33px"
-    }
+  root: {
+    maxWidth: 514,
+  },
+  fields: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContents: "center",
+  },
+  button1: {
+    marginLeft: "24px",
+  },
+  button2: {
+    marginLeft: "33px",
+  },
 }));
 
-function UserInput() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+function UserInput(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
-    };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // do something with name and email
-        console.log("Name: ", name);
-        console.log("Email: ", email);
-    };
+  // Define a function to generate a random id
+  const generateId = () => {
+    // Use Math.random and Math.floor to get a random number between 1 and 1000
+    return Math.floor(Math.random() * 1000) + 1;
+  };
 
-    const handleClear = () => {
-        setName("");
-        setEmail("");
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // do something with name and email
+    const data = { Name: name, Email: email, id: generateId() };
 
-    const classes = useStyles();
+    props.onFormSubmit(data);
 
-    return (<form onSubmit={handleSubmit} className={classes.root}>
-        <Box className={classes.fields}>
-            <Typography sx={{mr: 2, display: 'inline-block', width: '119px'}}>Test</Typography>
-            <TextField
-                value={name}
-                onChange={handleNameChange}
-                fullWidth
-                margin="normal"
-            />
-        </Box>
-        <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <Typography sx={{mr: 2, display: 'inline-block', width: '119px'}}>Email</Typography>
-            <TextField
-                // label="Email"
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                fullWidth
-                margin="normal"
+    console.log("Name: ", name);
+    console.log("Email: ", email);
+  };
 
-            />
-        </Box>
-        <Box sx={{marginTop: "103.88px"}}>
-            <Button type="submit" variant="contained" color="primary" style={{marginLeft: 24}}>
-                Submit
-            </Button>
-            <Button type="button" variant="contained" color="secondary" onClick={handleClear}
-                    style={{marginLeft: 33}}
-            >
-                Clear
-            </Button>
-        </Box>
-    </form>);
+  const handleClear = () => {
+    setName("");
+    setEmail("");
+  };
+
+  const classes = useStyles();
+
+  return (
+    <form onSubmit={handleSubmit} className={classes.root}>
+      <Box className={classes.fields}>
+        <Typography sx={{ mr: 2, display: "inline-block", width: "119px" }}>
+          Test
+        </Typography>
+        <TextField
+          value={name}
+          onChange={handleNameChange}
+          fullWidth
+          margin="normal"
+        />
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <Typography sx={{ mr: 2, display: "inline-block", width: "119px" }}>
+          Email
+        </Typography>
+        <TextField
+          // label="Email"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          fullWidth
+          margin="normal"
+        />
+      </Box>
+      <Box sx={{ marginTop: "103.88px" }}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          style={{ marginLeft: 24 }}
+        >
+          Submit
+        </Button>
+        <Button
+          type="button"
+          variant="contained"
+          color="secondary"
+          onClick={handleClear}
+          style={{ marginLeft: 33 }}
+        >
+          Clear
+        </Button>
+      </Box>
+    </form>
+  );
 }
 
 export default UserInput;
